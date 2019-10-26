4ccd3462e54a38a1754520f61c7b17b66a2dd58a
Sam Connolly <samconnolly94@gmail.com>
2019-10-26 11:39:53 +0100

    Updated tests to check that number validation is occurring.

--- src/com/bbc/numerals/test/NumeralTests.java
+++ src/com/bbc/numerals/test/NumeralTests.java
@@ -1,6 +1,7 @@
 package com.bbc.numerals.test;
 
 import com.bbc.numerals.converters.NumeralConverter;
+import com.bbc.numerals.exceptions.InvalidNumberException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,62 +15,85 @@ public class NumeralTests {
     }
 
     @Test
-    public void One() {
+    public void Zero() throws Exception {
+        try {
+            Assert.assertEquals(numeralConverter.generate(0), "INVALID");
+        } catch (InvalidNumberException e) {
+            // Success. we expected an exception to be thrown
+            return;
+        }
+
+        throw new Exception("Should have thrown exception while trying to generate an invalid number");
+    }
+
+    @Test
+    public void One() throws InvalidNumberException {
         Assert.assertEquals(numeralConverter.generate(1), "I");
     }
 
     @Test
-    public void Two() {
+    public void Two() throws InvalidNumberException {
         Assert.assertEquals(numeralConverter.generate(2), "II");
     }
 
     @Test
-    public void Three() {
+    public void Three() throws InvalidNumberException {
         Assert.assertEquals(numeralConverter.generate(3), "III");
     }
 
     @Test
-    public void Four() {
+    public void Four() throws InvalidNumberException {
         Assert.assertEquals(numeralConverter.generate(4), "IV");
     }
     @Test
-    public void Five() {
+    public void Five() throws InvalidNumberException {
         Assert.assertEquals(numeralConverter.generate(5), "V");
     }
 
     @Test
-    public void Ten() {
+    public void Ten() throws InvalidNumberException {
         Assert.assertEquals(numeralConverter.generate(10), "X");
     }
 
     @Test
-    public void Twenty() {
+    public void Twenty() throws InvalidNumberException {
         Assert.assertEquals(numeralConverter.generate(20), "XX");
     }
 
     @Test
-    public void OneHundredNine() {
+    public void OneHundredNine() throws InvalidNumberException {
         Assert.assertEquals(numeralConverter.generate(109), "CIX");
     }
 
     @Test
-    public void Ninety() {
+    public void Ninety() throws InvalidNumberException {
         Assert.assertEquals(numeralConverter.generate(90), "XC");
     }
 
     @Test
-    public void ThreeThousandNineHundredNinetyNine(){
-        Assert.assertEquals(numeralConverter.generate(3999), "MMMCMXCIX");
+    public void OneThousandFourHundredNinetyNine() throws InvalidNumberException {
+        Assert.assertEquals(numeralConverter.generate(1499), "MCDXCIX");
     }
 
     @Test
-    public void TwoThousandFourHundredThirtyTwo() {
+    public void TwoThousandFourHundredThirtyTwo() throws InvalidNumberException {
         Assert.assertEquals(numeralConverter.generate(2432), "MMCDXXXII");
     }
 
     @Test
-    public void OneThousandFourHundredNinetyNine() {
-        Assert.assertEquals(numeralConverter.generate(1499), "MCDXCIX");
+    public void ThreeThousandNineHundredNinetyNine() throws InvalidNumberException {
+        Assert.assertEquals(numeralConverter.generate(3999), "MMMCMXCIX");
     }
 
+    @Test
+    public void FourThousand() throws Exception {
+        try {
+            Assert.assertEquals(numeralConverter.generate(4000), "INVALID");
+        } catch (InvalidNumberException e) {
+            // Success. we expected an exception to be thrown
+            return;
+        }
+
+        throw new Exception("Should have thrown exception while trying to generate an invalid number");
+    }
 }
